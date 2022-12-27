d5befbe3f47d2ab9781b7d59ae8463aa7305bb42
lucaslinyker <lucaslinyker@outlook.com>
2022-12-27 18:01:14 -0300

    .

--- desafios/d2/index.js
+++ desafios/d2/index.js
@@ -1,4 +1,7 @@
-function clique(n) {
-    document.getElementById(`#sect${n}`).style.backgroundColor = blue;
-    console.log(n)
+function clique(id) {
+    document.getElementById('sect1').style.opacity = '50%';
+    document.getElementById('sect2').style.opacity = '50%';
+    document.getElementById('sect3').style.opacity = '50%';
+
+    document.getElementById(id).style.opacity = '100%';
 }
